30634da5a060a3164cb9ca1ac02e1d5ea62b8970
effieep <epapafili2003@gmail.com>
2026-01-10 17:04:27 +0200

    vet availability works

--- src/pages/vet/vetRequestRdvz.js
+++ src/pages/vet/vetRequestRdvz.js
@@ -1,5 +1,16 @@
+import { Typography } from '@mui/material';
+import { useAuth } from "../../auth/AuthContext";
+
+
 const VetRequestRdvz = () => {
-    return <div>Vet Request Rdvz Page</div>;
+    const {user, isLoggedIn} = useAuth();
+    return ( isLoggedIn && user?.role === 'vet' ?
+        (<div>Vet requests Page</div>) 
+    : 
+    ( <Typography variant="h6" color="error" textAlign="center" sx={{ mt: 10 }}>
+        Παρακαλώ συνδεθείτε ως Κτηνίατρος για να δείτε τα αιτήματα για ραντεβού.
+    </Typography>)
+    );
 };
 
 export default VetRequestRdvz;
--- src/pages/vet/vetScheduledRdvz.js
+++ src/pages/vet/vetScheduledRdvz.js
@@ -1,5 +1,15 @@
+import { Typography } from '@mui/material';
+import { useAuth } from "../../auth/AuthContext";
+
 const VetScheduledRdvz = () => {
-    return <div>Vet Scheduled Rdvz Page</div>;
+    const {user, isLoggedIn} = useAuth();
+    return ( isLoggedIn && user?.role === 'vet' ?
+        (<div>Vet Scheduled Rdvz Page</div>) 
+    : 
+    ( <Typography variant="h6" color="error" textAlign="center" sx={{ mt: 10 }}>
+        Παρακαλώ συνδεθείτε ως Κτηνίατρος για να δείτε τα προγραμματισμένα ραντεβού.
+    </Typography>)
+    );
 };
 
 export default VetScheduledRdvz;
